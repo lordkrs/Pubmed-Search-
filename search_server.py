61f697b99394d6522dcc56214e4d3c65fb8ddcdf
karthikshastry <karthikshastry@karthavya.com>
2019-06-23 03:25:02 +0530

    clinical trails added

--- search_server.py
+++ search_server.py
@@ -9,6 +9,8 @@ from multiprocessing import process
 import openpyxl
 from bottle import abort, request, static_file, run, route
 import sys
+from clinical_trials import Trials
+import zipfile
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
@@ -22,12 +24,14 @@ PUBMED_DATE_QUERY = '+AND+("{}"[PDat] : "{}"[PDat])'
 #PUBMED_DOWNLOAD_CSV =  "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={}&rettype=fasta&retmode=xml&api_key=6f63b0b5ec41afd50bed862a0d61ff0ae709"
 PUBMED_DOWNLOAD_CSV =  "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&rettype=fasta&retmode=xml&api_key=6f63b0b5ec41afd50bed862a0d61ff0ae709"
 #Date_format:YYYY/MM/DD
-headers = ["GM Universal Code", "Full Name", "Author Match","Authorship_Position", "Publication_Type", "Mesh_Headings","Title","URL", "Query Used",
+pubmed_headers = ["GM Universal Code", "Full Name", "Author Match","Authorship_Position", "Publication_Type", "Mesh_Headings","Title","URL", "Query Used",
           "Description","Details","ShortDetails", "Affiliation","Resource","Type","Identifiers","Db","EntrezUID","Properties", "Author_Count", "Abstract_Text"]
 
+trails_headers = ['GM Universal Code', 'Full Name', 'NCT ID', 'URL', 'Verification Status', 'Query Used', 'Trial Name', "Trial Type",'Trial Phase' , 'Overall Status',
+ 'Start Date', 'End Date', 'Conditions', 'Interventions', 'Role', 'Facility', 'Region', 'Other Associates', 'Organizations', 'Lead Sponsor(s)']
 
 
-def create_xlsx(data=None, data_list=[], local=False):
+def create_xlsx(data=None, data_list=[], local=False,headers=pubmed_headers):
     file_name = str(uuid.uuid4())+".xlsx"
     workbook = xlsxwriter.Workbook(temp_path + os.path.sep + file_name)
     worksheet = workbook.add_worksheet()
@@ -239,7 +243,7 @@ def do_upload():
     
     from_date = request.forms.get('from_date') if request.forms.get('from_date') else None
     to_date = request.forms.get('to_date') if request.forms.get('to_date') else None
-
+    search_type = request.forms.get("search_type") if request.forms.get('search_type') else None
 
     if from_date and to_date:
         if datetime.datetime.strptime(from_date, "%Y-%m-%d") > datetime.datetime.strptime(to_date, "%Y-%m-%d"):
@@ -286,7 +290,7 @@ def do_upload():
 
 
 @route("/search", method='POST')
-def search_citations(name=None, initial=None, lastname=None, firstname=None, universal_id=None,from_date=None, to_date=None,  records_per_page="400", local_searh=False):
+def search_citations(name=None, search_type="Pubmed",initial=None, lastname=None, firstname=None, universal_id=None,from_date=None, to_date=None,  records_per_page="400", local_searh=False):
     try:
         if not local_searh:
             name = request.forms.get('Name')  if request.forms.get('Name') else None
@@ -297,8 +301,12 @@ def search_citations(name=None, initial=None, lastname=None, firstname=None, uni
             initial = request.forms.get('Initial')  if request.forms.get('Initial') else None
             lastname = request.forms.get('Lastname')  if request.forms.get('Lastname') else None
             firstname = request.forms.get("FirstName") if request.forms.get('FirstName') else None
+            search_type = request.forms.get("search_type") if request.forms.get('search_type') else None
         
-        
+        if search_type == "Clinical Trails":
+            file_ = clinical_trails(name, universal_id, lastname=lastname, initial=initial, firstname=firstname)
+            return static_file(file_, temp_path, download=file_)
+
         url = None
         if not name:
             raise Exception("Name is madatory field")
@@ -475,6 +483,153 @@ def download_csv(query_data=None, local=False):
         abort(500, "Exception occurred: {}".format(ex))
 
 
+def get_interventions(interventions_list):
+    data = ""
+    i = 0
+    if type(interventions_list) == dict:
+        interventions_list = [interventions_list]
+    for interventions in interventions_list:
+        data += interventions["intervention_name"]
+        i += 1
+        if i != len(interventions_list):
+            data += " | "
+
+    return data
+
+def get_role(overall_list):
+    if type(overall_list) == dict:
+        overall_list = [overall_list]
+    data = ""
+    i = 0
+    for v in overall_list:
+        data += v.get("role","")
+        i += 1
+        if i != len(overall_list):
+            data += " | "
+    return data
+
+def get_facilities(facilities, name, last_name=None):
+    if type(facilities) == dict:
+        facilities = [facilities]
+    search_name = name
+    if last_name:
+        search_name = [last_name]
+    else:
+        search_name = name.split(" ")
+
+
+    for facility in facilities:
+        for name_ in search_name:
+            if name_ in facility["facility"]["name"]:
+                return facility["facility"]["name"], "{}_{}_{}".format(facility["facility"]["address"].get("country"),facility["facility"]["address"].get("city"), facility["facility"]["address"].get("zip"))
+        
+    return "", ""
+
+def get_other_associates(facilities):
+    if type(facilities) == dict:
+        facilities = [facilities]
+    data = ""
+    for facility in facilities:
+        data += facility["facility"]["name"] + "|"
+    return data
+
+def get_sponsers(sponsers_list):
+    if type(sponsers_list) == dict:
+        sponsers_list = [sponsers_list]
+    data = ""
+    for sponser in sponsers_list:
+        data += sponser["lead_sponsor"]["agency"]
+    return data
+
+def get_start_date(date_):
+    if type(date_) == dict:
+        return date_["#text"]
+    else:
+        return date_
+
+def clinical_trails(name, _uuid, lastname=None, initial=None, firstname=None):
+    print("name:{},lastname:{}".format(name,lastname))
+    t = Trials()
+    zip_folder_path = os.path.join(temp_path,str(uuid.uuid4()))
+    if not os.path.exists(zip_folder_path):
+        os.makedirs(zip_folder_path)
+    file_path = os.path.join(zip_folder_path,str(uuid.uuid4())+ ".zip")
+    zip_file_ = open(file_path,"wb")
+    if lastname:
+        zip_data = t.download(search_term=lastname)
+    else:
+        zip_data = t.download(search_term=name)
+    zip_file_.write(zip_data)
+    zip_file_.close()
+    import zipfile
+    zip_ref = zipfile.ZipFile(file_path, 'r')
+    zip_ref.extractall(zip_folder_path)
+    zip_ref.close()
+    os.remove(file_path)
+    files = os.listdir(zip_folder_path)
+    print("Search results--->{}".format(len(files)))
+    clinical_trails_data = []
+    for file_ in files:
+        xml_file = os.path.join(zip_folder_path, file_)
+        xml_file_obj = open(xml_file,"r")
+        xml_data = xml_file_obj.read()
+        json_data = json.loads(xml_to_json(xml_data))
+        clinical_trails_data.append(json_data)
+        xml_file_obj.close()
+        os.remove(xml_file)
+    os.rmdir(zip_folder_path)
+
+    xlsx_data = []
+    for data in clinical_trails_data:
+        clinical_study = data["clinical_study"]
+        form_data = {}
+        form_data["GM Universal Code"] = _uuid
+        form_data["Full Name"] = name
+        form_data["NCT ID"] = clinical_study["id_info"]["nct_id"]
+        form_data["URL"] = clinical_study["required_header"]["url"]
+        form_data["Verification Status"] = "Probable"
+        if lastname:
+            form_data["Query Used"] = lastname
+        else:
+            form_data["Query Used"] = name
+        form_data["Trial Name"] = clinical_study["brief_title"]     
+        form_data["Trial Phase"] = clinical_study.get("phase","N/A")
+
+        form_data["Overall Status"] = clinical_study["overall_status"]
+        form_data["Start Date"] = get_start_date(clinical_study["start_date"])
+        if clinical_study.get("completion_date"):
+            form_data["End Date"] = clinical_study["completion_date"]["#text"]
+        else:
+            form_data["End Date"] = clinical_study["primary_completion_date"]["#text"]
+        form_data["Conditions"] = " | ".join(clinical_study["condition"]) if type(clinical_study["condition"]) == list else clinical_study["condition"]
+        if clinical_study.get("intervention"):
+            form_data["Interventions"] = get_interventions(clinical_study["intervention"])
+        else:
+            form_data["Interventions"] = ""
+        if clinical_study.get("overall_official"):
+            form_data["Role"] =  get_role(clinical_study["overall_official"])
+        else:
+            form_data["Role"] = ""
+        form_data["Associate Type"] = "N/A"
+        if clinical_study.get("location"):
+            form_data['Facility'],form_data['Region'] = get_facilities(clinical_study["location"], name,lastname)
+            form_data["Other Associates"] = get_other_associates(clinical_study["location"])
+        else:
+            form_data['Facility'] = ""
+            form_data["Other Associates"] = ""
+            form_data['Region'] = ""
+        form_data["Trial Type"] = clinical_study.get("study_type","")
+        form_data["Organizations"] = clinical_study["source"]
+        form_data["Lead Sponsor(s)"] = get_sponsers(clinical_study["sponsors"])
+        xlsx_data.append(form_data)
+    
+    if not xlsx_data:
+        raise Exception("Error occured while comunicating with clinical Trials")
+
+    file_name = create_xlsx(data=xlsx_data, local=False, headers=trails_headers)
+
+    return file_name
+        
 @route("/clear_tmp",method="POST")
 def clear_tmp():
     files = os.listdir(temp_path)
@@ -489,4 +644,5 @@ def serve_css(css_file):
 def serve_web():
     return static_file("index.html", os.path.dirname(os.path.abspath(__file__)))
 
-run(host="0.0.0.0",port=8090)
+if __name__ == "__main__":
+    run(host="0.0.0.0",port=8090)
